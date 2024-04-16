bfa134b1c014899f3bb0cde3f89b10df49a64558
Christian Rémillard <christian.remillard@gmail.com>
2024-04-16 12:58:34 -0400

    feat: Ajout d'un composant pour la rétroaction usager

---
+++ src/components/RetroactionUsager.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Container } from '@mui/material'
+
+export default function RetroactionUsager({ formId = '20e7ff6aa4aeadadbea8aa877a806bb9' }) {
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = `https://umontreal.libwizard.com/form_loader.php?id=${formId}&noheader=0`
+    script.async = true
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
+  return (
+    <Container>
+      <div id={`form_${formId}`} style={{ display: 'inline-block', height: '258px' }}></div>
+    </Container>
+  )
+}
+
+RetroactionUsager.propTypes = {
+  formId: PropTypes.string.isRequired,
+}
