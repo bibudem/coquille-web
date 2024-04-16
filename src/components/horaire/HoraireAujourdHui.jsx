a722b8602658b25ee6ff599f19faf2d9153ee68a
Christian Rémillard <christian.remillard@gmail.com>
2024-04-16 11:31:58 -0400

    feat: Ajout d'un composant <HoraireAujourdHui /> et d'une page /widget-horaire démo

---
+++ src/components/horaire/HoraireAujourdHui.jsx
@@ -0,0 +1,81 @@
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import useSWR from 'swr'
+import Link from '@/components/Link'
+
+/**
+ * @typedef {('am'|'antenne-paramedicale'|'cartotheque'|'marcel-laurin'|'ch'|'conservation'|'cs'|'dr'|'ed'|'gp'|'ki'|'laval'|'mi'|'mu'|'mv'|'pa'|'pb'|'py'|'sa'|'sciences'|'ss'|'youville')} CodeBib
+ */
+
+/**
+ * @param {CodeBib} bib
+ */
+
+export default function HoraireAujourdHui({ bib, service = 'https://api.bib.umontreal.ca/horaires', lienHoraires = false }) {
+  const today = new Date()
+  const currentDay = today.getDay()
+
+  async function fetcher(bib) {
+    const dateDebut = new Date()
+    dateDebut.setDate(today.getDate() - currentDay)
+    const debut = dateDebut.toISOString().substring(0, 10)
+
+    const res = await fetch(`${service}/${bib}?debut=${debut}&fin=P7D`)
+    const data = await res.json()
+    const horaires = data.evenements.filter((horaire) => horaire.service === 'regulier')
+
+    return {
+      sommaire: horaires[currentDay].sommaire,
+      details: horaires.map((horaire) => horaire.sommaire),
+    }
+  }
+
+  const jours = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi']
+
+  const { data, error, isLoading } = useSWR(bib, fetcher)
+
+  if (error) {
+    return <div>failed to load</div>
+  }
+
+  if (isLoading) {
+    return <div>loading...</div>
+  }
+
+  console.log(data)
+  // render data
+  return (
+    <Accordion
+      component="span"
+      disableGutters
+      elevation={0}
+      sx={{
+        display: 'inline-block',
+        border: (theme) => `1px solid ${theme.palette.divider}`,
+      }}
+    >
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ textTransform: 'capitalize' }}>
+        Aujourd'hui: {data.sommaire}
+      </AccordionSummary>
+      <AccordionDetails>
+        <table>
+          {data.details.map((horaire, i) => {
+            const fontWeight = i === currentDay ? 'bold' : null
+            return (
+              <tr key={i}>
+                <td style={{ textTransform: 'capitalize', paddingRight: '0.5rem', fontWeight }}>{jours[i]}</td>
+                <td style={{ textTransform: 'capitalize', fontWeight }}>{horaire}</td>
+              </tr>
+            )
+          })}
+        </table>
+
+        {lienHoraires && (
+          <Link to={`/horaires`} sx={{ fontSize: 'smaller', pt: 1.5, display: 'block' }}>
+            Consultez tous les horaires &gt;
+          </Link>
+        )}
+      </AccordionDetails>
+    </Accordion>
+  )
+}
