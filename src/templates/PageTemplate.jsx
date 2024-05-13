62ccf2946ef8d09a8f41e63ef890c63cf74f6110
Christian Rémillard <christian.remillard@gmail.com>
2024-05-13 16:53:28 -0400

    feat: Ajout du composant <bib-retroaction-usager>

--- src/templates/PageTemplate.jsx
+++ src/templates/PageTemplate.jsx
@@ -5,17 +5,22 @@ import AppBar from '@/components/AppBar'
 import Footer from '@/components/Footer'
 import Link from '@/components/Link'
 import SEO from '@/components/SEO'
-import RetroactionUsager from '@/components/RetroactionUsager'
 
 const shortcodes = { Link, Accordion, AccordionDetails, AccordionSummary, Button, Box, Divider, Tab, Tabs } // Provide common components here
 
 export default function PageTemplate({ children }) {
   return (
     <MDXProvider components={shortcodes}>
+      <udem-urgence></udem-urgence>
       <AppBar />
       <bib-avis bouton-fermer />
-      <Container component="main">{children}</Container>
-      <RetroactionUsager />
+      <Container component="main">
+        {children}
+
+        <Box component="aside" className="article-footer" sx={{ pt: '2rem' }}>
+          <bib-retroaction-usager />
+        </Box>
+      </Container>
       <Footer />
     </MDXProvider>
   )
@@ -37,6 +42,8 @@ export function Head({ pageContext, location }) {
   return (
     <SEO title={frontmatter?.title} pathname={pathname}>
       <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0/dist/bib-avis.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0/dist/bib-retroaction-usager.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0/dist/udem-urgence.js"></script>
     </SEO>
   )
 }
