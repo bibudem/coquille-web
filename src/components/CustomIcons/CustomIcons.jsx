caa4085c9be88f3fa4f67bda182f4ccfc5f2f982
Christian Rémillard <christian.remillard@gmail.com>
2025-01-29 10:17:08 -0500

    feat(icons): Ajout d'une icône custom pour Sofia (SofiaIcon)

---
+++ src/components/CustomIcons/CustomIcons.jsx
@@ -0,0 +1,7 @@
+import { SvgIcon, useTheme } from '@mui/material'
+import SofiaSVG from '@/icons/Sofia.svg'
+
+export function SofiaIcon(color, ...props) {
+  
+  return <SvgIcon {...props} component={SofiaSVG} inheritViewBox sx={ theme => ({color: color ?? theme.palette.grey['700']})} />
+}
