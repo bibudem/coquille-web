afb79a6e3fc1f5dbdffe3ff10efc0c0892103e03
Christian Rémillard <christian.remillard@gmail.com>
2025-02-04 11:45:47 -0500

    feat(icons): Ajout de l'icône de menu burger personnalisé `BurgerIcon`.

--- src/components/CustomIcons/CustomIcons.jsx
+++ src/components/CustomIcons/CustomIcons.jsx
@@ -1,6 +1,11 @@
 import { SvgIcon } from '@mui/material'
 import SofiaSVG from '@/icons/sofia.svg'
+import BurgerSVG from '@/icons/burger.svg'
 
-export function SofiaIcon(color, ...props) {
+export function SofiaIcon(color, sx, ...props) {
   return <SvgIcon {...props} component={SofiaSVG} inheritViewBox sx={(theme) => ({ color: color ?? theme.palette.grey['700'] })} />
+}
+
+export function BurgerIcon(color, sx, ...props) {
+  return <SvgIcon {...props} component={BurgerSVG} inheritViewBox sx={(theme) => ({ color: color ?? theme.palette.grey['700'] })} />
 }
