ed9f157fb89a559760287c54b77b928f5bf9ae9d
Christian Rémillard <christian.remillard@gmail.com>
2025-02-04 11:06:27 -0500

    Corrections mineures

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material/styles'
 import Container from '@mui/material/Container'
-import Breadcrumbs from '@mui/material/Breadcrumbs'
+import MUIBreadcrumbs from '@mui/material/Breadcrumbs'
 import Link from '@mui/material/Link'
 import { HouseLine } from '@phosphor-icons/react'
 
@@ -16,7 +16,7 @@ function handleClick(event) {
   console.info('You clicked a breadcrumb.', event)
 }
 
-export default function BasicBreadcrumbs({ crumbs }) {
+export default function Breadcrumbs({ crumbs }) {
   console.log('crumbs:', crumbs)
 
   return (
@@ -28,14 +28,14 @@ export default function BasicBreadcrumbs({ crumbs }) {
       }}
     >
       <div onClick={handleClick}>
-        <Breadcrumbs aria-label="Fil d'ariane">
+        <MUIBreadcrumbs aria-label="Fil d'ariane">
           {crumbs.map(({ pathname, crumbLabel }, index) => (
             <StyledBreadcrumb key={index} href={pathname}>
               {index === 0 ? <HouseLine size="1.125rem" /> : ''}
               {crumbLabel}
             </StyledBreadcrumb>
           ))}
-        </Breadcrumbs>
+        </MUIBreadcrumbs>
       </div>
     </Container>
   )
