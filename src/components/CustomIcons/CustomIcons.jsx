7570b497c310ec272d581fd5daab731fc597e83c
Christian Rémillard <christian.remillard@gmail.com>
2025-02-04 09:48:09 -0500

    fix(icons): Correction de la casse du nom de fichier Sofia.svg pour sofia.svg. Ceci brisait le build en environnement *nix, en particulier ceux de Netlify.

--- src/components/CustomIcons/CustomIcons.jsx
+++ src/components/CustomIcons/CustomIcons.jsx
@@ -1,5 +1,5 @@
 import { SvgIcon } from '@mui/material'
-import SofiaSVG from '@/icons/Sofia.svg'
+import SofiaSVG from '@/icons/sofia.svg'
 
 export function SofiaIcon(color, ...props) {
   return <SvgIcon {...props} component={SofiaSVG} inheritViewBox sx={(theme) => ({ color: color ?? theme.palette.grey['700'] })} />
