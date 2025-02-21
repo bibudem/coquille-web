6184a87b1574360606ac594cba5e190a7bde608f
Christian Rémillard <christian.remillard@gmail.com>
2025-02-21 12:13:48 -0500

    fix(component): Card1: renommage de la propriété linkText pour moreText

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -14,13 +14,23 @@ const StyledTitle = styled('div')({
   lineHeight: 1.2,
 })
 
-const StyledLinkText = styled('div')({
+const StyledMoreText = styled('div')({
   fontSize: '1rem',
   fontWeight: 500,
   lineHeight: 1.5,
 })
 
-export default function Card1({ title, IconComponent, linkText, href, ...rest }) {
+/**
+ * Card1 component that renders a card with a title, icon, link text, and link.
+ * @param {Object} props - The component props.
+ * @param {string} props.title - The title of the card.
+ * @param {React.ComponentType} props.IconComponent - The icon component to be displayed.
+ * @param {string} props.moreText - The text to be displayed for the link.
+ * @param {string} props.href - The URL of the link.
+ * @param {any} props.rest - Any additional props to be passed to the component.
+ * @returns {React.ReactElement} - The Card1 component.
+ */
+export default function Card1({ title, IconComponent, moreText, href, ...rest }) {
   if (typeof href !== 'string') {
     throw new Error('The `href`prop must be a string')
   }
@@ -90,7 +100,7 @@ export default function Card1({ title, IconComponent, linkText, href, ...rest })
             }}
           >
             <Grid size="grow">
-              <StyledLinkText>{linkText}</StyledLinkText>
+              <StyledMoreText>{moreText}</StyledMoreText>
             </Grid>
             <Grid size="auto">
               <StyledLinkIcon weight="light" />
