d1c19d424c1b81317d84ecea2cfee1b9f561aeac
Christian Rémillard <christian.remillard@gmail.com>
2025-03-06 11:59:06 -0500

    feat(component): Carousel1: la propriété `description`peut prendre un fragment React en plus de simple texte

--- src/components/Carousel1.jsx
+++ src/components/Carousel1.jsx
@@ -1,9 +1,22 @@
 import { Children } from 'react'
-import { Typography, useTheme } from '@mui/material'
+import { styled, Typography, useTheme } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import Button from '@/components/Button'
 import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from './CustomIcons.jsx'
 
+const Div = styled('div')()
+
+/**
+ * A React component that renders a carousel with a title, description, and optional "more" link.
+ *
+ * @param {object} props - The component props.
+ * @param {string} props.title - The title of the carousel.
+ * @param {string|React.ReactNode} props.description - The description of the carousel, which can be a string or a React element.
+ * @param {string} [props.moreText] - The text for the "more" link.
+ * @param {string} [props.moreLink] - The URL for the "more" link.
+ * @param {React.ReactNode} [props.children] - The content to be displayed in the carousel.
+ * @returns {React.ReactElement} - The Carousel1 component.
+ */
 export default function Carousel1({ title, description, moreText, moreLink = '#', ...rest }) {
   const { sx, children, ...props } = rest
   const theme = useTheme()
@@ -28,16 +41,27 @@ export default function Carousel1({ title, description, moreText, moreLink = '#'
           {title}
         </Typography>
         <Grid container>
-          <Grid size="grow">
-            <Typography
-              variant="body1"
-              sx={{
-                pb: 2,
+          <Grid 
+            size="grow"
+            sx={{
+              '.MuiTypography-root': {
+                fontSize: '1.25rem!important',
                 ...variableWidthStyles,
-              }}
-            >
-              {description}
-            </Typography>
+              }
+            }}
+          >
+            <Div sx={variableWidthStyles}>
+              {typeof description === 'string' ? (    
+                <Typography
+                  variant="body1"
+                  component="div"
+                >
+                  {description}
+                </Typography>
+                ) : (
+                description
+                )}
+              </Div>
           </Grid>
           <Grid container size="auto" spacing="10px" sx={{ alignItems: 'flex-end', pl: 4 }}>
             <ArrowLeftCircleIcon fontSize={50} color="#0057ac" />
@@ -46,11 +70,13 @@ export default function Carousel1({ title, description, moreText, moreLink = '#'
         </Grid>
       </Grid>
       <Grid container size={12} spacing="10px">
-        {Children.toArray(children).map((child, index) => (
-          <Grid key={index} size="auto">
-            {child}
-          </Grid>
-        ))}
+        {Children.toArray(children).map((child, index) => {
+          return (
+            <Grid key={index} size="auto">
+              {child}
+            </Grid>
+          )
+        })}
       </Grid>
       {moreLink && moreText && (
         <Grid size={12}>
