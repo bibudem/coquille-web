d28cbe7972080f4ea35557631bc178905249d0f3
Christian Rémillard <christian.remillard@gmail.com>
2025-03-06 11:19:55 -0500

    feat(component): Carousel1: changement de div pour h2 pour le title, ajout de mediaquery pour la largeur de title/description lorsque plus grand que breakpoints.sm

--- src/components/Carousel1.jsx
+++ src/components/Carousel1.jsx
@@ -1,21 +1,41 @@
 import { Children } from 'react'
-import { Typography } from '@mui/material'
+import { Typography, useTheme } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import Button from '@/components/Button'
 import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from './CustomIcons.jsx'
 
 export default function Carousel1({ title, description, moreText, moreLink = '#', ...rest }) {
   const { sx, children, ...props } = rest
+  const theme = useTheme()
+
+  const variableWidthStyles = {
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '69rem',
+    },
+  }
 
   return (
     <Grid container spacing="45px">
       <Grid size={12}>
-        <Typography variant="display5" component="div" sx={{ pb: 4 }}>
+        <Typography
+          variant="display5"
+          component="h2"
+          sx={{
+            pb: 4,
+            ...variableWidthStyles,
+          }}
+        >
           {title}
         </Typography>
         <Grid container>
           <Grid size="grow">
-            <Typography variant="body1" sx={{ pb: 2 }}>
+            <Typography
+              variant="body1"
+              sx={{
+                pb: 2,
+                ...variableWidthStyles,
+              }}
+            >
               {description}
             </Typography>
           </Grid>
