5a8e51f4c7bc9fa10613d58062c0f4e44f7ea246
Christian Rémillard <christian.remillard@gmail.com>
2025-03-07 12:10:54 -0500

    feat(icons): Ajout de l'icône ArrowUpRightCircleIcon dans le module CustomIcons

--- src/components/CustomIcons.jsx
+++ src/components/CustomIcons.jsx
@@ -3,8 +3,27 @@ import SofiaSVG from '@/icons/sofia.svg'
 import BurgerSVG from '@/icons/burger.svg'
 import ArrowLeftCircle from '@/icons/arrow-left-circle.svg'
 import ArrowRightCircle from '@/icons/arrow-right-circle.svg'
+import ArrowUpRightCircle from '@/icons/arrow-up-right-circle.svg'
 
-export function SofiaIcon(color, sx, ...props) {
+function iconComponentFactory(IconComponent) {
+  return function IconComponentFactory({ color, size = 50, sx, ...props }) {
+    console.log('typeof size', typeof size, size)
+    return (
+      <SvgIcon
+        inheritViewBox
+        {...props}
+        component={IconComponent}
+        sx={(theme) => ({
+          color: color ?? theme.palette.grey['700'],
+          fontSize: typeof size === 'string' ? size : `${size}px`,
+          ...sx,
+        })}
+      />
+    )
+  }
+}
+
+export function SofiaIcon(color, size, sx, ...props) {
   return <SvgIcon {...props} component={SofiaSVG} inheritViewBox sx={(theme) => ({ color: color ?? theme.palette.grey['700'], ...sx })} />
 }
 
@@ -19,3 +38,9 @@ export function ArrowLeftCircleIcon(color, sx, ...props) {
 export function ArrowRightCircleIcon(color, sx, ...props) {
   return <SvgIcon {...props} component={ArrowRightCircle} inheritViewBox sx={(theme) => ({ color: color ?? theme.palette.grey['700'], ...sx })} />
 }
+
+export function ArrowUpRightCircleIcon(color, sx, ...props) {
+  return <SvgIcon {...props} component={ArrowUpRightCircle} inheritViewBox sx={(theme) => ({ color: color ?? theme.palette.grey['700'], ...sx })} />
+}
+
+// export const ArrowUpRightCircleIcon = iconComponentFactory(ArrowUpRightCircle)
