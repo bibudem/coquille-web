0174e212ddc11cbda6dd8dc296216202b752a27b
Christian Rémillard <christian.remillard@gmail.com>
2025-03-17 16:55:52 -0400

    fix(component): MenuLatteral: ajout d'un effet de transition lorsqu'on scroll

--- src/components/MenuLatteral/MenuLatteral.jsx
+++ src/components/MenuLatteral/MenuLatteral.jsx
@@ -1,4 +1,4 @@
-import { Stack, styled, Typography } from '@mui/material'
+import { Stack, styled, useScrollTrigger } from '@mui/material'
 import { CalendarPlus, Chats, ClockCountdown, Lifebuoy } from '@phosphor-icons/react'
 import { SofiaIcon } from '@/components/CustomIcons'
 
@@ -7,14 +7,37 @@ const StyledNav = styled(Stack)(({ theme }) => ({
   padding: 20,
   backgroundColor: theme.vars.palette.bleuPrincipal.main, // var(--mui-palette-primary-main)
   color: theme.vars.palette.bleuPrincipal.contrastText,
+  boxShadow: '0 4px 16px 0 #22293040',
   borderRadius: '12px 0 0 12px',
   listStyle: 'none',
   gap: 15.69,
+  pointerEvents: 'auto',
+  transitionProperty: `transform`,
 }))
 
-const StyledNavItem = styled('li')(({ theme }) => ({}))
+const StyledNavItem = styled('li')({})
+
+const A = styled('a')(({ theme }) => ({
+  color: 'inherit',
+  textDecoration: 'none',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  alignItems: 'center',
+  gap: '0.98088rem',
+}))
+
+const StyledNavItemText = styled('div')({
+  fontSize: '1rem',
+  fontWeight: 400,
+  lineHeight: '1.6rem',
+})
 
 export default function MenuLatteral() {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 200,
+  })
+
   function handleClick(event) {
     alert('Action à définir')
   }
@@ -32,46 +55,49 @@ export default function MenuLatteral() {
         right: 0,
         bottom: 0,
         justifyContent: 'center',
-        width: '4rem',
+        // width: '4rem',
         zIndex: theme.zIndex.drawer,
         pointerEvents: 'none',
+        color: '#fff',
       })}
     >
       <StyledNav
         component="ul"
-        sx={{
-          pointerEvents: 'auto',
-        }}
+        sx={(theme) => ({
+          transform: trigger ? 'translateX(170px)' : 'translateX(0)',
+          transitionTimingFunction: theme.transitions.easing.md3[trigger ? 'emphasizedDecelerate' : 'emphasizedAccelerate'],
+          transitionDuration: `${theme.transitions.duration.md3[trigger ? 'medium4' : 'short4']}ms`,
+        })}
       >
         <StyledNavItem>
-          <a href="https://umontreal.on.worldcat.org/discovery?lang=fr">
+          <A href="https://umontreal.on.worldcat.org/discovery?lang=fr">
             <SofiaIcon color="#fff" />
-            <Typography sx={{ display: 'none' }}>Sofia</Typography>
-          </a>
+            <StyledNavItemText>Sofia</StyledNavItemText>
+          </A>
         </StyledNavItem>
         <StyledNavItem>
-          <a href="/horaires">
+          <A href="/horaires">
             <ClockCountdown color="#fff" size={24} />
-            <Typography sx={{ display: 'none' }}>Horaires</Typography>
-          </a>
+            <StyledNavItemText>Horaires</StyledNavItemText>
+          </A>
         </StyledNavItem>
         <StyledNavItem>
-          <a href="https://calendrier.bib.umontreal.ca/r">
+          <A href="https://calendrier.bib.umontreal.ca/r">
             <CalendarPlus color="#fff" size={24} />
-            <Typography sx={{ display: 'none' }}>Réserver une salle</Typography>
-          </a>
+            <StyledNavItemText>Réserver une salle</StyledNavItemText>
+          </A>
         </StyledNavItem>
         <StyledNavItem>
-          <a href="#" onClick={handleClick}>
+          <A href="#" onClick={handleClick}>
             <Lifebuoy color="#fff" size={24} />
-            <Typography sx={{ display: 'none' }}>Soutien informatique</Typography>
-          </a>
+            <StyledNavItemText>Soutien informatique</StyledNavItemText>
+          </A>
         </StyledNavItem>
         <StyledNavItem>
-          <a href="#" onClick={handleClick}>
+          <A href="#" onClick={handleClick}>
             <Chats color="#fff" size={24} />
-            <Typography sx={{ display: 'none' }}>Clavarder</Typography>
-          </a>
+            <StyledNavItemText>Clavarder</StyledNavItemText>
+          </A>
         </StyledNavItem>
       </StyledNav>
     </Stack>
