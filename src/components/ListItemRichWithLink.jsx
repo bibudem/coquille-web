900f3716022d284dec9f212f4e13ab30fdca9de3
Christian Rémillard <christian.remillard@umontreal.ca>
2025-03-18 13:26:12 -0400

    wip

--- src/components/ListItemRichWithLink.jsx
+++ src/components/ListItemRichWithLink.jsx
@@ -4,6 +4,36 @@ import Button from '@/components/Button'
 import Div from '@/components/utils/Div'
 import isInternalLink from '../utils/internLink.js'
 
+function Header({ children }) {
+  return (
+    <h3
+      style={{
+        margin: 0,
+        padding: 0,
+        fontSize: '2rem',
+        fontWeight: 500,
+        lineHeight: 1.2,
+      }}
+    >
+      {children}
+    </h3>
+  )
+}
+
+function Description({ children }) {
+  return (
+    <div
+      style={{
+        padding: '.75rem 0 0',
+        fontSize: '17px',
+        lineHeight: 1.4,
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
 export default function ListItemRichWithLink({ title, description, href, linkText = null, ...rest }) {
   const { sx, ...props } = rest
 
@@ -18,57 +48,49 @@ export default function ListItemRichWithLink({ title, description, href, linkTex
     <>
       <ListItem disableGutters>
         <ListItemButton component="a" href={href}>
-          <ListItemText 
+          <ListItemText
+            disableTypography
             primary={
               <Div
                 sx={(theme) => ({
                   [theme.breakpoints.up('md')]: {
-                    width: '83.3333%'
+                    width: '83.3333%',
                   },
                   [theme.breakpoints.up('lg')]: {
-                    width: '66.6667%'
-                  }
+                    width: '66.6667%',
+                  },
                 })}
               >
-                <Typography component="h3" variant="h3">{title}</Typography>
+                <Header>{title}</Header>
               </Div>
             }
             secondary={
               <Div
                 sx={(theme) => ({
                   [theme.breakpoints.up('md')]: {
-                    width: '83.3333%'
+                    width: '83.3333%',
                   },
                   [theme.breakpoints.up('lg')]: {
-                    width: '66.6667%'
-                  }
+                    width: '66.6667%',
+                  },
                 })}
               >
-                <Typography>{description}</Typography>
+                <Description>{description}</Description>
               </Div>
             }
           />
-          {
-            linkText ? (
-              <Button primary href={href} onClick={onButtonClick}>{linkText}</Button>
-            ) : (
-              <IconButton
-                color='bleuFonce'
-                href={href}
-              >
-                {
-                  isInternal ? (
-                    <ArrowRight size={24} />
-                  ) : (
-                    <ArrowUpRight size={24} />
-                  )
-                }
-              </IconButton>
-            )
-          }
+          {linkText ? (
+            <Button primary href={href} onClick={onButtonClick}>
+              {linkText}
+            </Button>
+          ) : (
+            <IconButton href={href} sx={{ color: 'inherit' }}>
+              {isInternal ? <ArrowRight size={24} color="currentColor" /> : <ArrowUpRight size={24} color="currentColor" />}
+            </IconButton>
+          )}
         </ListItemButton>
       </ListItem>
-      <Divider component="li" sx={{borderColor: '#2178C4'}} />
+      <Divider component="li" sx={{ borderColor: 'currentColor' }} />
     </>
   )
 }
