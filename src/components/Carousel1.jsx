16258aa1038050ea9460416faf88f8b5a394cb95
Christian Rémillard <christian.remillard@gmail.com>
2025-03-31 16:20:02 -0400

    feat(component): Carousel1: la largeur de chaque item d'un carroussel est maintenant calculée à partir du contenu de l'item.

--- src/components/Carousel1.jsx
+++ src/components/Carousel1.jsx
@@ -1,8 +1,8 @@
-import { Children, useEffect, useMemo, useState } from 'react'
+import { Children, useEffect, useMemo, useRef, useState } from 'react'
 import { IconButton, Typography, useTheme } from '@mui/material'
 import Grid from '@mui/material/Grid2'
-import * as carousel from '@zag-js/carousel'
-import { normalizeProps, useMachine } from '@zag-js/react'
+import TinySlider from 'tiny-slider-react'
+import 'tiny-slider/dist/tiny-slider.css'
 import Button from '@/components/Button'
 import Div from '@/components/utils/Div'
 import { useSmall } from '@/hooks/use-small'
@@ -16,8 +16,6 @@ const slidesPerPagPerBreakpoint = {
   lg: 4,
   xl: 4,
 }
-
-let idCounter = 1
 
 /**
  * A React component that renders a carousel with a title, description, and optional "more" link.
@@ -33,20 +31,70 @@ let idCounter = 1
 export default function Carousel1({ title, description, moreText, moreLink = '#', ...rest }) {
   const { sx, children, ...props } = rest
   const currentBreakpoint = useBreakpoint()
+  const sliderRef = useRef(null)
+  const [sliderState, setSliderState] = useState(null)
   const [showNavigation, setShowNavigation] = useState(true)
-  const [slidesPerPage, setSlidesPerPage] = useState(4)
+  const [slidesPerPage, setSlidesPerPage] = useState(1)
+  const [isFirst, setIsFirst] = useState(true)
+  const [isLast, setIsLast] = useState(false)
   const theme = useTheme()
   const isSmall = useSmall()
   const slides = Children.toArray(children)
-  const id = useMemo(() => idCounter++, [])
+  const [api, setApi] = useState(null)
 
-  const service = useMachine(carousel.machine, { id, slideCount: slides.length, spacing: '10px', slidesPerPage: slidesPerPagPerBreakpoint[currentBreakpoint.key] })
-  const { getRootProps, getControlProps, getPrevTriggerProps, getNextTriggerProps, getItemGroupProps, getItemProps } = carousel.connect(service, normalizeProps)
+  const settings = {
+    autoWidth: true,
+    gutter: 10,
+    lazyload: true,
+    loop: false,
+    nav: false,
+    mouseDrag: true,
+    controls: false, // remove built-in nav buttons
+    responsive: {
+      [theme.breakpoints.values.xs]: {
+        items: 1,
+      },
+      [theme.breakpoints.values.sm]: {
+        items: 2,
+      },
+      [theme.breakpoints.values.md]: {
+        items: 3,
+      },
+      [theme.breakpoints.values.lg]: {
+        items: 4,
+      },
+    },
+    slideBy: 'page',
+  }
 
   const variableWidthStyles = {
     [theme.breakpoints.up('md')]: {
       maxWidth: '69rem',
     },
+  }
+
+  useEffect(() => {
+    if (sliderRef.current) {
+      setApi(sliderRef.current)
+      setSliderState(sliderRef.current.slider.getInfo())
+    }
+  }, [])
+
+  useEffect(() => {
+    if (sliderState) {
+      const lastPage = Math.ceil(sliderState.slideCountNew / sliderState.slideBy) - 1
+      const currentPage = Math.ceil(sliderState.index / sliderState.slideBy)
+      setIsFirst(currentPage === 0)
+      setIsLast(currentPage === lastPage)
+    }
+  }, [sliderState])
+
+  function goTo(direction) {
+    if (api) {
+      api.slider.goTo(direction)
+    } else {
+      console.error('Carousel api not available')
+    }
   }
 
   useEffect(() => {
@@ -58,78 +106,74 @@ export default function Carousel1({ title, description, moreText, moreLink = '#'
   }, [slidesPerPage, slides])
 
   return (
-    <div {...getRootProps()}>
-      <Div sx={{ '--slide-item-size': 'calc(96% / var(--slides-per-page) - var(--slide-spacing) * (var(--slides-per-page) - 1) / var(--slides-per-page))' }}>
-        <Grid container spacing="45px">
-          <Grid size={12}>
-            <Div
+    <div>
+      <Grid container spacing="45px">
+        <Grid size={12}>
+          <Div
+            sx={{
+              pb: 4,
+              ...variableWidthStyles,
+              '.MuiTypography-root': {
+                fontSize: '2.25rem!important',
+                fontWeight: 400,
+                lineHeight: 1,
+              },
+            }}
+          >
+            {typeof title === 'string' ? <Typography component="h2">{title}</Typography> : title}
+          </Div>
+          <Grid container direction={isSmall ? 'column' : 'row'}>
+            <Grid
+              size="grow"
               sx={{
-                pb: 4,
-                ...variableWidthStyles,
                 '.MuiTypography-root': {
-                  fontSize: '2.25rem!important',
-                  fontWeight: 400,
-                  lineHeight: 1,
+                  fontSize: '1.25rem!important',
+                  ...variableWidthStyles,
                 },
               }}
             >
-              {typeof title === 'string' ? <Typography component="h2">{title}</Typography> : title}
-            </Div>
-            <Grid container direction={isSmall ? 'column' : 'row'}>
-              <Grid
-                size="grow"
-                sx={{
-                  '.MuiTypography-root': {
-                    fontSize: '1.25rem!important',
-                    ...variableWidthStyles,
-                  },
-                }}
-              >
-                <Div sx={variableWidthStyles}>
-                  {typeof description === 'string' ? (
-                    <Typography variant="body1" component="div">
-                      {description}
-                    </Typography>
-                  ) : (
-                    description
-                  )}
-                </Div>
+              <Div sx={variableWidthStyles}>
+                {typeof description === 'string' ? (
+                  <Typography variant="body1" component="div">
+                    {description}
+                  </Typography>
+                ) : (
+                  description
+                )}
+              </Div>
+            </Grid>
+            {showNavigation && (
+              <Grid container size="auto" spacing="10px" sx={{ ...(isSmall ? { justifyContent: 'flex-end' } : { alignItems: 'flex-end', pl: 4 }) }}>
+                <IconButton color="primary" aria-label="précédent" sx={{ fontSize: 50 }} onClick={() => goTo('prev')} disabled={isFirst}>
+                  <ArrowLeftCircleIcon fontSize={50} />
+                </IconButton>
+                <IconButton color="primary" aria-label="suivant" sx={{ fontSize: 50 }} edge="end" onClick={() => goTo('next')} disabled={isLast}>
+                  <ArrowRightCircleIcon fontSize={50} />
+                </IconButton>
               </Grid>
-              {showNavigation && (
-                <Grid container size="auto" spacing="10px" sx={{ ...(isSmall ? { justifyContent: 'flex-end' } : { alignItems: 'flex-end', pl: 4 }) }} {...getControlProps()}>
-                  <IconButton color="primary" aria-label="précédent" sx={{ fontSize: 50 }} {...getPrevTriggerProps()}>
-                    <ArrowLeftCircleIcon fontSize={50} />
-                  </IconButton>
-                  <IconButton color="primary" aria-label="suivant" sx={{ fontSize: 50 }} edge="end" {...getNextTriggerProps()}>
-                    <ArrowRightCircleIcon fontSize={50} />
-                  </IconButton>
-                </Grid>
-              )}
-            </Grid>
+            )}
           </Grid>
-          <Div sx={{ minWidth: '100%' }} {...getItemGroupProps()}>
-            {Children.toArray(children).map((child, index) => {
-              return (
-                <SnapSliderItem key={index} {...getItemProps({ index })}>
-                  {child}
-                </SnapSliderItem>
-              )
-            })}
-          </Div>
-          {moreLink && moreText && (
-            <Grid size={12}>
-              <Button primary href={moreLink}>
-                {moreText}
-              </Button>
-            </Grid>
-          )}
         </Grid>
-      </Div>
+        <Div sx={{ minWidth: '100%' }}>
+          <TinySlider ref={sliderRef} settings={settings} onIndexChanged={setSliderState}>
+            {Children.toArray(children).map((child, index) => {
+              return <SliderItem key={index}>{child}</SliderItem>
+            })}
+          </TinySlider>
+        </Div>
+        {moreLink && moreText && (
+          <Grid size={12}>
+            <Button primary href={moreLink}>
+              {moreText}
+            </Button>
+          </Grid>
+        )}
+      </Grid>
     </div>
   )
 }
 
-function SnapSliderItem({ children, ...props }) {
+function SliderItem({ children, ...props }) {
   return (
     <Div xs={{ display: 'flex', flexShrink: 0 }} {...props}>
       {children}
