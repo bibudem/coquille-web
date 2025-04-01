f30e479676592c182b7f7cb1768ae24ffeb2ded8
Christian Rémillard <christian.remillard@umontreal.ca>
2025-04-01 16:34:26 -0400

    wip

--- src/components/_layout/Debug.jsx
+++ src/components/_layout/Debug.jsx
@@ -24,7 +24,7 @@ export default function Debug() {
 
   useEffect(() => {
     const newResolution = queries.find((query) => query.match)
-    if (resolution?.key !== newResolution.key) {
+    if (newResolution && resolution?.key !== newResolution.key) {
       setResolution(newResolution)
     }
   }, queries)
