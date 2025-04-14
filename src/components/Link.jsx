9628bc2f185572e159a9bea10e41fbfcdd38abbe
Christian Rémillard <christian.remillard@gmail.com>
2025-04-14 15:14:17 -0400

    feat(component): Link: ajout d'une propriété `Icon`

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,8 +1,15 @@
+import { forwardRef } from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 import { Link as MuiLink } from '@mui/material'
 import { unstable_styleFunctionSx } from '@mui/system'
 import styled from '@emotion/styled'
-import { forwardRef } from 'react'
+import { ArrowRight } from '@phosphor-icons/react'
+
+const iconStyles = {
+  display: 'inline-flex',
+  alignItems: 'center',
+  gap: '.375em',
+}
 
 const StyledA = styled('a')(unstable_styleFunctionSx)
 
@@ -11,24 +18,30 @@ const StyledA = styled('a')(unstable_styleFunctionSx)
 // pass it only to GatsbyLink
 // export default function Link({ children, to, ...rest }) {
 const Link = forwardRef(function Link(props, ref) {
-  const { children, to, ...rest } = props
+  const { children, sx, Icon, iconProps, to, ...rest } = props
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
   const isInternal = /^\//.test(to)
+  const styles = Icon ? { ...iconStyles } : {}
+  const _iconProps = { size: '1.125rem', color: 'currentColor', ...iconProps }
+
+  const icon = Icon && typeof Icon === 'boolean' ? <ArrowRight {..._iconProps} /> : <Icon {..._iconProps} />
 
   // Use Gatsby Link for internal links, and <a> for others
   if (!isInternal) {
     return (
-      <StyledA ref={ref} href={to} {...rest}>
+      <StyledA ref={ref} href={to} sx={{ ...styles, ...sx }} {...rest}>
         {children}
+        {Icon && icon}
       </StyledA>
     )
   }
 
   return (
-    <MuiLink ref={ref} component={GatsbyLink} to={to} {...rest}>
+    <MuiLink ref={ref} component={GatsbyLink} to={to} sx={{ ...styles, ...sx }} {...rest}>
       {children}
+      {Icon && icon}
     </MuiLink>
   )
 })
