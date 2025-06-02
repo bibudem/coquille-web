f5ec7eae3e67467b7ec9269b493b0841b923296d
Christian Rémillard <christian.remillard@gmail.com>
2025-06-02 15:40:13 -0400

    fix

--- src/components/RichList.jsx
+++ src/components/RichList.jsx
@@ -3,8 +3,8 @@ import GridOffset from './utils/GridOffset'
 
 export default function ListRich({ offset = 0.5, children }) {
   return (
-    <GridOffset offset={offset}>
-      <List>{children}</List>
+    <GridOffset offset={offset} className="bib-comp-rich-list">
+      <List sx={{ padding: 0, width: '100%' }}>{children}</List>
     </GridOffset>
   )
 }
