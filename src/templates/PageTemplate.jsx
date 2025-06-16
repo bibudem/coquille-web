dfdf3f99915ab5194da152ba924355b3ea7a043b
Christian Rémillard <christian.remillard@umontreal.ca>
2025-06-16 14:53:21 -0400

    fix: Composants réutilisables: correction des appels de scripts afin de pointer vers la dernière version de la branche v1.x.x

--- src/templates/PageTemplate.jsx
+++ src/templates/PageTemplate.jsx
@@ -163,12 +163,12 @@ export function Head({ pageContext, location }) {
           <link rel="preload" href={`https://api.bib.umontreal.ca/horaires/?debut=${getLastSundayISODate()}&fin=P7D`} as="fetch" crossorigin="anonymous" />
         </>
       )}
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-gtm.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-avis.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-retroaction-usager.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/udem-urgence.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-consent.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-consent-preferences-btn.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-gtm.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-avis.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-retroaction-usager.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/udem-urgence.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-consent.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-consent-preferences-btn.min.js"></script>
 
       {noIndex && <meta name="robots" content="noindex, nofollow" />}
     </>
