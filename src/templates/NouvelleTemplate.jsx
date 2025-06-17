8690c951d8e159dda99fd74daef304eba8fa01e0
Christian Rémillard <christian.remillard@gmail.com>
2025-06-17 14:59:58 -0400

    refactor(component): Head: regroupement dans un seul fichier des éléments présents dans l'en-tête HTML des pages.

--- src/templates/NouvelleTemplate.jsx
+++ src/templates/NouvelleTemplate.jsx
@@ -10,17 +10,16 @@ import { QuickLinks, QuickLinksSm } from '@/components/_layout/QuickLinks'
 import Footer from '@/components/_layout/Footer/Footer'
 import Breadcrumbs from '@/components/_layout/Breadcrumbs/Breadcrumbs'
 import SkipTo from '@/components/_layout/SkipTo'
-import SEO from '@/components/_layout/SEO'
 import Debug from '@/components/_layout/Debug'
 import RetroactionUsager from '@/components/RetroactionUsager'
 import ConditionalWrapper from '@/components/utils/ConditionalWrapper'
+import { Head as HtmlHead } from '../components/_layout/HtmlHead'
 
 import { useSmall } from '@/hooks/use-small'
 
 import commonComponents from './commonComponents'
 
 function getCurrentPageLevel(location) {
-  // const pathname = location.pathname.endsWith('/') ? location.pathname.slice(1) : location.pathname
   return location.pathname.split('/').filter((item) => item).length
 }
 
@@ -116,7 +115,8 @@ function convertTZ(date) {
   return date.replace(/\.000Z$/i, '-05:00')
 }
 
-export function Head({ pageContext, location }) {
+export function Head(props) {
+  const { pageContext, location } = props
   const { frontmatter = {} } = pageContext
   const { date, title, newsUrl, authors, noIndex } = frontmatter
   const { pathname } = location
@@ -133,20 +133,8 @@ export function Head({ pageContext, location }) {
   }
 
   return (
-    <>
-      <html lang="fr" />
-      <SEO title={title} pathname={pathname} />
-      <bib-gtm></bib-gtm>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-gtm.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-avis.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-retroaction-usager.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/udem-urgence.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-consent.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-consent-preferences-btn.min.js"></script>
-
-      {noIndex && <meta name="robots" content="noindex, nofollow" />}
-
+    <HtmlHead {...props}>
       <script type="application/ld+json">{JSON.stringify(jsonld)}</script>
-    </>
+    </HtmlHead>
   )
 }
