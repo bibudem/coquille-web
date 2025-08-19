011d462453b1361b8786140ec3f0aba33c0c1362
msevigny <m.sevigny@umontreal.ca>
2025-08-19 11:03:48 -0400

    Modification des liens du menu burger et retrait des références à UdeM

--- src/components/_layout/SideNav/SideNavContent.jsx
+++ src/components/_layout/SideNav/SideNavContent.jsx
@@ -173,13 +173,18 @@ export default forwardRef(function SideNavContent({ close = noop, onClose = noop
       role="presentation"
     >
       <SideNavHeaderContainer>
-        <StyledLogoLink to="https://www.umontreal.ca/">
-          <LogoUdeM
+        <Link to="/" aria-label="Accueil" sx={{ display: 'block' }}>
+          <LogoBibUBlanc
             style={{
+              width: '170px',
               height: 'auto',
+              pointerEvents: 'none',
+              [theme.breakpoints.down('md')]: {
+                width: '90px',
+              },
             }}
           />
-        </StyledLogoLink>
+        </Link>
         <Box
           sx={{
             display: 'flex',
@@ -191,7 +196,7 @@ export default forwardRef(function SideNavContent({ close = noop, onClose = noop
           }}
         >
           <Button
-            href="https://monudem.umontreal.ca/"
+            href="/nous-joindre/"
             variant="outlined"
             sx={{
               color: theme.palette.common.white,
@@ -202,18 +207,17 @@ export default forwardRef(function SideNavContent({ close = noop, onClose = noop
               fontSize: '14px',
               [theme.breakpoints.down('md')]: {
                 fontSize: '12px',
-                padding: '5px 8px',
+                padding: '6px 12px',
                 '& .MuiButton-endIcon': {
                   marginLeft: '4px',
                   '& svg': {
-                    fontSize: '12px',
+                    fontSize: '20px',
                   },
                 },
               },
             }}
-            endIcon={<ArrowUpRightIcon size={28} />}
           >
-            Mon UdeM
+            Nous joindre
           </Button>
           <Button
             href="/nous-soutenir/"
@@ -305,29 +309,37 @@ export default forwardRef(function SideNavContent({ close = noop, onClose = noop
                 </NavList>
               </Nav>
           )}
-          <Nav aria-label="À propos" bg={true}>
-            <NavHeader>À propos</NavHeader>
+          <Nav aria-label="Les bibliothèques" bg={true}>
+            <NavHeader>Les bibliothèques</NavHeader>
             <NavList>
               <NavListItem href="/a-propos/notre-organisation/">Notre organisation</NavListItem>
+              <NavListItem href="/a-propos/mission-vision-valeur">Notre mission, notre vision, nos valeurs et nos objectifs</NavListItem>
               <NavListItem href="/a-propos/nos-collections">Nos collections</NavListItem>
-              <NavListItem href="/a-propos/mission-vision-valeur">Mission, vision, valeurs et objectifs</NavListItem>
-              <NavListItem href="/a-propos/rapports-annuels">Rapports annuels</NavListItem>
-              <NavListItem href="/a-propos/politiques-reglement">Politiques et règlement</NavListItem>
+              <NavListItem href="/a-propos/politiques-reglement">Nos politiques et règlement</NavListItem>
+              <NavListItem href="/a-propos/rapports-annuels">Nos rapports annuels</NavListItem>
               <NavListItem href="/nouvelles/">Nouvelles</NavListItem>
               <NavListItem href="/a-propos/carriere">Carrière</NavListItem>
+              <NavListItem href="/horaires">Nos heures d'ouverture</NavListItem>
+              <NavListItem href="/nous-joindre/">Nous joindre</NavListItem>
+
+
             </NavList>
           </Nav>
 
-          <Nav aria-label="Plateformes" bg={false}>
-            <NavHeader>Plateformes</NavHeader>
+          <Nav aria-label="Vos ressources" bg={false}>
+            <NavHeader>Vos ressources</NavHeader>
             <NavList>
-              <NavListItem href="https://studio.bib.umontreal.ca/">Studio•bib - écosystème numérique</NavListItem>
-              <NavListItem href="https://boite-outils.bib.umontreal.ca/c.php?g=739631">La boîte à outils - guides</NavListItem>
-              <NavListItem href="https://umontreal.on.worldcat.org/discovery?lang=fr">Sofia - outil de découverte</NavListItem>
+              <NavListItem href="https://calendrier.bib.umontreal.ca/r">Réservation de salles</NavListItem>
+              <NavListItem href="https://umontreal.on.worldcat.org/discovery?lang=fr">Sofia - accès aux collections</NavListItem>
               <NavListItem href="https://boite-outils.bib.umontreal.ca/az.php">Bases de données de A à Z</NavListItem>
-              <NavListItem href="https://umontreal.scholaris.ca/">Papyrus - dépôt institutionnel</NavListItem>
+              <NavListItem href="https://boite-outils.bib.umontreal.ca/">La boîte à outils - ressources et guides spécialisés</NavListItem>
+              <NavListItem href="https://studio.bib.umontreal.ca/">Studio•bib - soutien informatique et à la création numérique</NavListItem>
               <NavListItem href="https://geoapp.bibl.ulaval.ca/">GéoIndex - données géospatiales</NavListItem>
-              <NavListItem href="https://calypso.bib.umontreal.ca/">Calypso - objets numériques</NavListItem>
+              <NavListItem href="https://umontreal.scholaris.ca/">Papyrus - dépôt institutionnel</NavListItem>
+              <NavListItem href="https://calypso.bib.umontreal.ca/">Calypso - collections numérisées</NavListItem>
+              <NavListItem href="https://studio.bib.umontreal.ca/informatique/">Soutien informatique</NavListItem>
+
+
             </NavList>
           </Nav>
 
@@ -339,79 +351,12 @@ export default forwardRef(function SideNavContent({ close = noop, onClose = noop
               <NavListItem href="/obtenir/peb/">Prêt entre bibliothèques</NavListItem>
               <NavListItem href="/obtenir/frais-avis">Frais et avis de retard</NavListItem>
               <NavListItem href="/nous-joindre/suggestion-achat/">Suggestion d'achat</NavListItem>
+              <NavListItem href="/service-accessibilite/">Service accessibilité</NavListItem>
             </NavList>
           </Nav>
 
-          <Nav aria-label="Liens rapides" bg={false}>
-            <NavHeader sx={{ visibility: 'hidden' }}>Liens rapides</NavHeader>
-            <NavList>
-              <NavListItem href="https://umontreal.on.worldcat.org/discovery?lang=fr" icon={<SofiaIcon color="white" fontSize="24px" />}>
-                Sofia
-              </NavListItem>
-              <NavListItem href="/horaires" icon={<ClockCountdownIcon color="white" size={24} />}>
-                Horaires
-              </NavListItem>
-              <NavListItem href="https://calendrier.bib.umontreal.ca/r" icon={<CalendarPlusIcon color="white" size={24} />}>
-                Réserver une salle
-              </NavListItem>
-              <NavListItem href="https://studio.bib.umontreal.ca/informatique/" icon={<LifebuoyIcon color="white" size={24} />}>
-                Soutien informatique
-              </NavListItem>
-            </NavList>
-          </Nav>
+
         </Box>
-      </Box>
-
-      <Box
-        sx={{
-          backgroundColor: isMobile ? theme.palette.bleuFonce.main : 'transparent',
-          [theme.breakpoints.down('lg')]: {
-            borderTop: `1px solid ${theme.palette.common.white}`,
-          },
-        }}
-      >
-        <SideNavHeaderContainer>
-          <Box>
-            <Link to="/" aria-label="Accueil" sx={{ display: 'block' }}>
-              <LogoBibUBlanc
-                style={{
-                  width: '200px',
-                  height: 'auto',
-                  pointerEvents: 'none',
-                  [theme.breakpoints.down('md')]: {
-                    width: '90px',
-                  },
-                }}
-              />
-            </Link>
-          </Box>
-          <Box>
-            <Button
-              href="/nous-joindre/"
-              variant="outlined"
-              sx={{
-                color: theme.palette.common.white,
-                borderColor: theme.palette.grey[300],
-                '&:hover': {
-                  borderColor: theme.palette.grey[300],
-                },
-                fontSize: '14px',
-                [theme.breakpoints.down('md')]: {
-                  fontSize: '12px',
-                  padding: '6px 12px',
-                  '& .MuiButton-endIcon': {
-                    marginLeft: '4px',
-                    '& svg': {
-                      fontSize: '20px',
-                    },
-                  },
-                },
-              }}
-            >
-              Nous joindre
-            </Button>
-          </Box>
-        </SideNavHeaderContainer>
       </Box>
 
       {isMobile && (
