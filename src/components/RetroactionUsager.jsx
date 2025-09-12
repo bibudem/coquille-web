3acf372988547fd5dcf7ee9952a814a6931aa261
emirgc <emir.chouchane@umontreal.ca>
2025-09-11 22:04:04 -0400

    Ajout de la largeur responsive pour le composant RetroactionUsager.

--- src/components/RetroactionUsager.jsx
+++ src/components/RetroactionUsager.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { Alert } from '@mui/material';
 
 export default function RetroactionUsager() {
@@ -7,6 +6,10 @@ export default function RetroactionUsager() {
       severity="info"
       sx={{
         my: 4,
+        width: {
+          xs: '100%', // mobile
+          sm: '40%',  // à partir de sm (600px)
+        },
         '--bib-comp-retroaction-usager-title-weight': 600,
         '--md-sys-typescale-label-large-size': '.875em',
         backgroundColor: '#EEF4F7',
