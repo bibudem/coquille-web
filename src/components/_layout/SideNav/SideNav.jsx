9b39137b6afef50e84f13e99ac57d9832939a67d
bib-umontreal-admin <natalia.jabinschi@umontreal.ca>
2025-09-16 16:41:11 -0400

    Les sources pour le nouveau renfort du menu burger ne sont pas encore terminées, il reste des ajustements à faire pour les petits écrans.

--- src/components/_layout/SideNav/SideNav.jsx
+++ src/components/_layout/SideNav/SideNav.jsx
@@ -1,8 +1,24 @@
 import { Drawer } from '@mui/material'
+import { appBarHeight } from '../AppBar/TopAppBar'
 
 export default function SideNav({ children, ...props }) {
   return (
-    <Drawer variant="temporary" slotProps={{ paper: { sx: { backgroundColor: 'bleuFonce.main', color: 'bleu200.main' } } }} {...props}>
+    <Drawer
+      variant="temporary"
+      slotProps={{
+        paper: {
+          sx: {
+            backgroundColor: '#222930',
+            color: '#fff',
+            padding: '2rem',
+            top: appBarHeight, // reste sous l’AppBar
+            height: 'auto',    // hauteur ajustée au contenu
+            maxHeight: `calc(100% - ${appBarHeight})`, // jamais plus que l’écran
+          },
+        },
+      }}
+      {...props}
+    >
       {children}
     </Drawer>
   )
