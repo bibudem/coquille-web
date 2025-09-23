83ebb356188156f82b7cbc21ac149a351b3df8ff
Christian Rémillard <christian.remillard@gmail.com>
2025-09-23 09:32:13 -0400

    Revert "Revert "Mise à jour du menu burger selon le nouveau style Figma""

--- src/components/_layout/SideNav/SideNav.jsx
+++ src/components/_layout/SideNav/SideNav.jsx
@@ -1,8 +1,25 @@
 import { Drawer } from '@mui/material'
+import { appBarHeight } from '../AppBar/TopAppBar'
 
 export default function SideNav({ children, ...props }) {
   return (
-    <Drawer variant="temporary" slotProps={{ paper: { sx: { backgroundColor: 'bleuFonce.main', color: 'bleu200.main' } } }} {...props}>
+    <Drawer
+      variant="temporary"
+      slotProps={{
+        paper: {
+          sx: {
+            backgroundColor: '#222930',
+            color: '#fff',
+            padding: '2rem',
+            width:'85vw',
+            top: appBarHeight,
+            height: 'auto',   
+            maxHeight: `calc(100% - ${appBarHeight})`,
+          },
+        },
+      }}
+      {...props}
+    >
       {children}
     </Drawer>
   )
