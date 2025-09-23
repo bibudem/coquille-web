6a18b1da952ef82c98634d7a6941ec1fab5cf4b2
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2025-09-23 14:18:08 -0400

    Corrections des liens

--- src/components/_layout/SideNav/SideNavContent.jsx
+++ src/components/_layout/SideNav/SideNavContent.jsx
@@ -82,7 +82,7 @@ const cards = [
     title: 'Boîte à outils',
     links: [
       { label: 'Base de données A-Z', href: 'https://boite-outils.bib.umontreal.ca/az.php' },
-      { label: 'Guides disciplinaires', href: 'https://boite-outils.bib.umontreal.ca/srch.php?q=Guides+disciplinaires' },
+      { label: 'Guides disciplinaires', href: 'https://boite-outils.bib.umontreal.ca/disciplines' },
       { label: 'Comment citer', href: 'https://boite-outils.bib.umontreal.ca/citer' },
       { label: 'Droit d\'auteur', href: 'https://boite-outils.bib.umontreal.ca/recherche/droit-auteur' },
     ],
@@ -98,6 +98,7 @@ const cards = [
       { label: 'Papyrus – dépôt institutionnel', href: 'https://umontreal.scholaris.ca/' },
       { label: 'GéoIndex – Données géospatiales', href: 'https://geoapp.bibl.ulaval.ca/' },
       { label: 'Calypso – Objets numériques', href: 'https://calypso.bib.umontreal.ca/' },
+      { label: 'Service Accessibilité', href: 'https://bib.umontreal.ca/service-accessibilite/' },
     ],
     //cta: { label: 'Toutes les ressources', href: '#' },
     footerColor: '#52B782',
