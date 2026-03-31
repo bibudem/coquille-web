4ca317de4ca9b3122a6307d29ff4029e262eeb72
Christian Rémillard <christian.remillard@umontreal.ca>
2026-03-31 13:26:16 -0400

    Changement du numéro de version des script importés de jsdelivr pour fins de débogage

--- src/components/_layout/HtmlHead.jsx
+++ src/components/_layout/HtmlHead.jsx
@@ -9,13 +9,13 @@ export function Head({ pageContext, location, ...rest }) {
     <>
       <html lang="fr" />
       <SEO title={title} pathname={pathname} />
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-gtm.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-avis.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-retroaction-usager.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/udem-urgence.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-consent.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-consent-preferences-btn.min.js"></script>
-      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-clarity.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/bib-gtm.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/bib-avis.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/bib-retroaction-usager.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/udem-urgence.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/bib-consent.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/bib-consent-preferences-btn.min.js"></script>
+      <script type="module" src="https://cdn.jsdelivr.net/gh/bibudem/ui@1.3.10/dist/bib-clarity.min.js"></script>
       <script type="module" src="https://corvee.bib.umontreal.ca/loader.js" defer></script>
 
       {noIndex && <meta name="robots" content="noindex, nofollow" />}
